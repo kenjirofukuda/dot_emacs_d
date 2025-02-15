b5eeba9820b1b2162d564034da7696aa484ca5bb
kenjirofukuda <kenjirofukuda@gmail.com>
2025-02-15 19:26:45 +0900

    add multiple-cursors keybind

--- experminal-init.el
+++ experminal-init.el
@@ -700,4 +700,15 @@ middle"
   (when (file-exists-p jam-mode-path)
     (load-file jam-mode-path)))
 
+(use-package multiple-cursors
+  :ensure t)
+
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->")         'mc/mark-next-like-this)
+(global-set-key (kbd "C-<")         'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<")     'mc/mark-all-like-this)
+(global-set-key (kbd "C-\"")        'mc/skip-to-next-like-this)
+(global-set-key (kbd "C-:")         'mc/skip-to-previous-like-this)
+
 (recentf-open-files)
+(put 'downcase-region 'disabled nil)
